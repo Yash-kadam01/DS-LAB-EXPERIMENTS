e728e3355b6938aafbf99d02fcffbf5702f66b32
Yash Kadam <yashsunilkadam23@gmail.com>
2026-08-22 22:08:50 +0530

    fix: untrack nested repository and upload C++ files

---
+++ Experiment_0/Experiment_0_A/Experiment_0_A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter numbers a: ";
+    cin>>a;
+    cout<<"Enter numbers b: ";
+    cin>>b;
+    cout<<"Before swapping a: "<<a<<endl;
+    cout<<"Before swapping b: "<<b<<endl;
+    int temp=a;
+    a=b;
+    b=temp;
+    cout<<"After swapping a: "<<a<<endl;
+    cout<<"After swapping b: "<<b<<endl;
+    return 0;
+}//Completed and Checked by Professor..
